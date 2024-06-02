2d8b5beea6cd4be69d2473532de24a01247d9b8d
Cristina Jesus <cristinaaajs@gmail.com>
2024-06-02 16:42:46 -0300

    Atualização: Entrega 2 Site Final

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('open_Trailer').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('trailer').style.display = 'block';
+    document.getElementById('open_Trailer').style.display = 'none';
+})
